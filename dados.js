53bda79639f717fda45cd6e68d4df3c90e438e24
pedroaojunior <133039837+pedroaojunior@users.noreply.github.com>
2024-09-07 14:45:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+    {
+        titulo: "Neymar Jr",
+        descricao: "Neymar Jr, é um dos maiores jogadores de futebol da atualidade. Revelado pelo Santos, logo chamou a atenção do mundo com seu drible fácil, velocidade e finalização precisa.",
+        link: "https://www.youtube.com/watch?v=wrIoqo7dLoA",
+        tags: "al-hilal NeymarJr Neymar Craque DriblesDeGênioacante Barcelona seleção FutebolArte Estrangeiros "
+      },
+      {
+        titulo: "Vinicius Jr",
+        descricao: "Vinicius Jr, jovem promessa do futebol brasileiro, destacou-se no Flamengo antes de ser contratado pelo Real Madrid. Conhecido por sua velocidade e habilidade nos dribles, tem se consolidado como um dos principais atacantes do mundo.",
+        link: "https://www.youtube.com/watch?v=YJ_kg0p6Iks",
+        tags:"Velocidade Drible GolRelâmpago Boladeouro seleção Bola de Ouro Estrangeiros "
+      },
+      {
+        titulo: "Rodrygo Goes",
+        descricao: "Rodrygo Goes, outro talento revelado pelo Santos, também faz parte do elenco do Real Madrid. Sua versatilidade e finalização precisa o destacam como uma das maiores promessas do futebol mundial.",
+        link: "https://www.youtube.com/watch?v=k0CfTWSgE9Q",
+        tags:"Goleador ChutePotente Finalizador Atacante Meia Multifuncional seleção Estrangeiros jogadores"
+      },
+      {
+        titulo: "Raphinha",
+        descricao: "Raphinha, conhecido por sua habilidade e velocidade, se destacou no Leeds United antes de se transferir para o Barcelona. Sua adaptação ao futebol espanhol foi rápida e ele se tornou um dos principais jogadores do time.",
+        link: "https://www.youtube.com/watch/v=seuLinkDoRaphinha", // Substitua por um link válido
+        tags: "Habilidoso Velocidade Drible Atacante Alado Estrangeiros Barcelona seleção jogadores  "
+    },
+    {
+        titulo: "Endrick",
+        descricao: "Endrick, considerado uma das maiores promessas do futebol mundial, foi contratado pelo Real Madrid ainda muito jovem. Sua técnica apurada e faro de gol o destacam como um dos grandes talentos da nova geração.",
+        link: "https://www.youtube.com/watch?v=OkmVPgMUNeM", // Substitua por um link válido
+        tags: "Prodígio Gênio Jovem Talento Atacante Estrangeiro Seleção Palmeiras Estrangeirosjogadores "
+    },
+    ];
+    
+

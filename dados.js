6f326a051934e0c5dbaa2d8d081cac617f54cef6
pedroaojunior <133039837+pedroaojunior@users.noreply.github.com>
2024-09-12 21:55:45 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,39 +1,47 @@
 let dados = [
     {
-        titulo: "Neymar Jr",
+        titulo: "Neymar Jr  - Clube atual: Al-hilal",
         foto:"https://upload.wikimedia.org/wikipedia/commons/b/bb/Neymar_Jr._with_Al_Hilal%2C_3_October_2023_-_03_%28cropped%29.jpg",
         descricao: "Neymar Jr, é um dos maiores jogadores de futebol da atualidade. Revelado pelo Santos, logo chamou a atenção do mundo com seu drible fácil, velocidade e finalização precisa.",
         link: "https://www.youtube.com/watch?v=wrIoqo7dLoA",
+        wiki: "https://pt.wikipedia.org/wiki/Neymar",
         tags: "al-hilal NeymarJr Neymar Craque DriblesDeGênioacante Barcelona seleção FutebolArte Estrangeiros meninoney gênio"
       },
       {
-        titulo: "Vinicius Jr",
+        titulo: "Vinicius Jr - Clube atual: Real Madrid ",
         foto:"https://upload.wikimedia.org/wikipedia/commons/f/f3/Vinicius_Jr_2021.jpg",
         descricao: "Vinicius Jr, jovem promessa do futebol brasileiro, destacou-se no Flamengo antes de ser contratado pelo Real Madrid. Conhecido por sua velocidade e habilidade nos dribles, tem se consolidado como um dos principais atacantes do mundo.",
         link: "https://www.youtube.com/watch?v=YJ_kg0p6Iks",
+        wiki: "https://pt.wikipedia.org/wiki/Vinícius_Júnior",
         tags:"Velocidade Drible GolRelâmpago Boladeouro seleção Bola de Ouro Estrangeiros laliga malvadeza jogadores"
       },
       {
-        titulo: "Rodrygo Goes",
+        titulo: "Rodrygo Goes - Clube atual: Real Madrid ",
         foto:"https://upload.wikimedia.org/wikipedia/commons/0/05/Rodrygo_2023_%28cropped%29.jpg",
         descricao: "Rodrygo Goes, outro talento revelado pelo Santos, também faz parte do elenco do Real Madrid. Sua versatilidade e finalização precisa o destacam como uma das maiores promessas do futebol mundial.",
         link: "https://www.youtube.com/watch?v=k0CfTWSgE9Q",
+        wiki: "https://pt.wikipedia.org/wiki/Rodrygo",
         tags:"Goleador ChutePotente Finalizador Atacante Meia Multifuncional seleção Estrangeiros jogadores laliga"
       },
       {
-        titulo: "Raphinha",
+        titulo: "Raphinha  - Clube atual: Barcelona",
         foto:"https://upload.wikimedia.org/wikipedia/commons/f/fb/Raphinha.jpg",
         descricao: "Raphinha, conhecido por sua habilidade e velocidade, se destacou no Leeds United antes de se transferir para o Barcelona. Sua adaptação ao futebol espanhol foi rápida e ele se tornou um dos principais jogadores do time.",
         link: "https://www.youtube.com/watch/v=seuLinkDoRaphinha", // Substitua por um link válido
+        wiki: "https://pt.wikipedia.org/wiki/Raphinha",
         tags: "Habilidoso Velocidade Drible Atacante Alado Estrangeiros Barcelona seleção jogadores laliga "
     },
     {
-        titulo: "Endrick",
+        titulo: "Endrick - Clube atual: Real Madrid ",
         foto:"https://upload.wikimedia.org/wikipedia/commons/a/a1/Endrick-Palmeiras-Liverpool-abr24.jpg",
         descricao: "Endrick, considerado uma das maiores promessas do futebol mundial, foi contratado pelo Real Madrid ainda muito jovem. Sua técnica apurada e faro de gol o destacam como um dos grandes talentos da nova geração.",
         link: "https://www.youtube.com/watch?v=OkmVPgMUNeM", // Substitua por um link válido
+        wiki: "https://pt.wikipedia.org/wiki/Endrick",
         tags: "Prodígio Gênio Jovem Talento Atacante Estrangeiro Seleção Palmeiras Estrangeirosjogadores Bobby Charlton Bobby"
     },
     ];
+    
+
+
     
 
